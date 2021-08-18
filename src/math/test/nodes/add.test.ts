7847426d3b94a9688c33e070508a82eaf96312d8
gfnuko <xahhaepica@gmail.com>
2021-08-18 22:34:53 +0900

    Add differentiate test for Add & Sub

--- src/math/test/nodes/add.test.ts
+++ src/math/test/nodes/add.test.ts
@@ -1,4 +1,8 @@
 import { Add, Constant } from '../../nodes'
+import Parser from '../../parser'
+import { isSameTree } from '../common'
+
+const parser = new Parser()
 
 describe('nodes.Add', () => {
   const a = new Constant(3)
@@ -7,5 +11,18 @@ describe('nodes.Add', () => {
 
   test('evaluate', () => {
     expect(sum.evaluate({})).toBe(11)
+  })
+
+  test('differentiate', () => {
+    const { expression: expr0 } = parser.parse('sinh(x)')
+    const { expression: expr1 } = parser.parse('sin(x)')
+    const expr = new Add(expr0!, expr1!)
+
+    expect(
+      isSameTree(
+        expr.differentiate('x'),
+        new Add(expr0!.differentiate('x'), expr1!.differentiate('x'))
+      )
+    ).toBeTruthy()
   })
 })
--- src/math/test/nodes/sub.test.ts
+++ src/math/test/nodes/sub.test.ts
@@ -1,4 +1,8 @@
 import { Constant, Sub } from '../../nodes'
+import Parser from '../../parser'
+import { isSameTree } from '../common'
+
+const parser = new Parser()
 
 describe('nodes.Sub', () => {
   const a = new Constant(3)
@@ -7,5 +11,18 @@ describe('nodes.Sub', () => {
 
   test('evaluate', () => {
     expect(sum.evaluate({})).toBe(-5)
+  })
+
+  test('differentiate', () => {
+    const { expression: expr0 } = parser.parse('sinh(x)')
+    const { expression: expr1 } = parser.parse('sin(x)')
+    const expr = new Sub(expr0!, expr1!)
+
+    expect(
+      isSameTree(
+        expr.differentiate('x'),
+        new Sub(expr0!.differentiate('x'), expr1!.differentiate('x'))
+      )
+    ).toBeTruthy()
   })
 })
