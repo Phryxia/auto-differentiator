1d9fe2dba12a5c602917412cdc760e667eb04458
gfnuko <xahhaepica@gmail.com>
2021-08-18 23:46:44 +0900

    Add differentiate test for hyper trigonometry

--- src/math/test/nodes/hypertrigonometry.test.ts
+++ src/math/test/nodes/hypertrigonometry.test.ts
@@ -1,43 +1,88 @@
-import { Sinh, Cosh, Tanh, Csch, Sech, Coth, Variable } from '../../nodes'
+import {
+  Sinh,
+  Cosh,
+  Tanh,
+  Csch,
+  Sech,
+  Coth,
+  Variable,
+  Sub,
+  Constant,
+  Power,
+  Mul,
+} from '../../nodes'
+import { testNumericalSimilarity } from '../common'
+
+const sinh = new Sinh(new Variable('x'))
+const cosh = new Cosh(new Variable('x'))
+const tanh = new Tanh(new Variable('x'))
+const csch = new Csch(new Variable('x'))
+const sech = new Sech(new Variable('x'))
+const coth = new Coth(new Variable('x'))
 
 describe('nodes.sinh', () => {
   test('evaluate', () => {
-    const sinh = new Sinh(new Variable('x'))
     expect(sinh.evaluate({ x: 1 })).toBeCloseTo(Math.sinh(1))
+  })
+
+  test('differentiate', () => {
+    testNumericalSimilarity(sinh.differentiate('x'), cosh, -1, 1, 0.01)
   })
 })
 
 describe('nodes.cosh', () => {
   test('evaluate', () => {
-    const cosh = new Cosh(new Variable('x'))
     expect(cosh.evaluate({ x: 1 })).toBeCloseTo(Math.cosh(1))
+  })
+
+  test('differentiate', () => {
+    testNumericalSimilarity(cosh.differentiate('x'), sinh, -1, 1, 0.01)
   })
 })
 
 describe('nodes.tanh', () => {
   test('evaluate', () => {
-    const tanh = new Tanh(new Variable('x'))
     expect(tanh.evaluate({ x: 1 })).toBeCloseTo(Math.tanh(1))
+  })
+
+  test('differentiate', () => {
+    const expected = new Sub(Constant.ONE, new Power(tanh, new Constant(2)))
+    testNumericalSimilarity(tanh.differentiate('x'), expected, -1, 1, 0.01)
   })
 })
 
 describe('nodes.csch', () => {
   test('evaluate', () => {
-    const csch = new Csch(new Variable('x'))
     expect(csch.evaluate({ x: 1 })).toBeCloseTo(1 / Math.sinh(1))
+  })
+
+  test('differentiate', () => {
+    const expected = new Mul(Constant.MINUS_ONE, new Mul(csch, coth))
+    testNumericalSimilarity(csch.differentiate('x'), expected, 0.1, 2, 0.01)
   })
 })
 
 describe('nodes.sech', () => {
   test('evaluate', () => {
-    const sech = new Sech(new Variable('x'))
     expect(sech.evaluate({ x: 1 })).toBeCloseTo(1 / Math.cosh(1))
+  })
+
+  test('differentiate', () => {
+    const expected = new Mul(Constant.MINUS_ONE, new Mul(sech, tanh))
+    testNumericalSimilarity(sech.differentiate('x'), expected, -1, 1, 0.01)
   })
 })
 
 describe('nodes.coth', () => {
   test('evaluate', () => {
-    const coth = new Coth(new Variable('x'))
     expect(coth.evaluate({ x: 1 })).toBeCloseTo(1 / Math.tanh(1))
+  })
+
+  test('differentiate', () => {
+    const expected = new Mul(
+      Constant.MINUS_ONE,
+      new Power(csch, new Constant(2))
+    )
+    testNumericalSimilarity(coth.differentiate('x'), expected, 0.1, 2, 0.01)
   })
 })
