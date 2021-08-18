5ca77d62643e50957302329a1cef5815171e357a
gfnuko <xahhaepica@gmail.com>
2021-08-18 20:43:04 +0900

    Fix trigonometry & hyper trigonometry test for floating number

--- src/math/test/nodes/hypertrigonometry.test.ts
+++ src/math/test/nodes/hypertrigonometry.test.ts
@@ -3,41 +3,41 @@ import { Sinh, Cosh, Tanh, Csch, Sech, Coth, Variable } from '../../nodes'
 describe('nodes.sinh', () => {
   test('evaluate', () => {
     const sinh = new Sinh(new Variable('x'))
-    expect(sinh.evaluate({ x: 1 })).toBe(Math.sinh(1))
+    expect(sinh.evaluate({ x: 1 })).toBeCloseTo(Math.sinh(1))
   })
 })
 
 describe('nodes.cosh', () => {
   test('evaluate', () => {
     const cosh = new Cosh(new Variable('x'))
-    expect(cosh.evaluate({ x: 1 })).toBe(Math.cosh(1))
+    expect(cosh.evaluate({ x: 1 })).toBeCloseTo(Math.cosh(1))
   })
 })
 
 describe('nodes.tanh', () => {
   test('evaluate', () => {
     const tanh = new Tanh(new Variable('x'))
-    expect(tanh.evaluate({ x: 1 })).toBe(Math.tanh(1))
+    expect(tanh.evaluate({ x: 1 })).toBeCloseTo(Math.tanh(1))
   })
 })
 
 describe('nodes.csch', () => {
   test('evaluate', () => {
     const csch = new Csch(new Variable('x'))
-    expect(csch.evaluate({ x: 1 })).toBe(1 / Math.sinh(1))
+    expect(csch.evaluate({ x: 1 })).toBeCloseTo(1 / Math.sinh(1))
   })
 })
 
 describe('nodes.sech', () => {
   test('evaluate', () => {
     const sech = new Sech(new Variable('x'))
-    expect(sech.evaluate({ x: 1 })).toBe(1 / Math.cosh(1))
+    expect(sech.evaluate({ x: 1 })).toBeCloseTo(1 / Math.cosh(1))
   })
 })
 
 describe('nodes.coth', () => {
   test('evaluate', () => {
     const coth = new Coth(new Variable('x'))
-    expect(coth.evaluate({ x: 1 })).toBe(1 / Math.tanh(1))
+    expect(coth.evaluate({ x: 1 })).toBeCloseTo(1 / Math.tanh(1))
   })
 })
--- src/math/test/nodes/trigonometry.test.ts
+++ src/math/test/nodes/trigonometry.test.ts
@@ -4,7 +4,7 @@ describe('nodes.sin', () => {
   test('evaluate', () => {
     const sin = new Sin(new Variable('x'))
     expect(sin.evaluate({ x: 1 })).toBe(Math.sin(1))
-    expect(sin.evaluate({ x: Math.PI })).toBe(0)
+    expect(sin.evaluate({ x: Math.PI })).toBeCloseTo(0)
   })
 })
 
@@ -12,7 +12,7 @@ describe('nodes.cos', () => {
   test('evaluate', () => {
     const cos = new Cos(new Variable('x'))
     expect(cos.evaluate({ x: 1 })).toBe(Math.cos(1))
-    expect(cos.evaluate({ x: Math.PI })).toBe(-1)
+    expect(cos.evaluate({ x: Math.PI })).toBeCloseTo(-1)
   })
 })
 
@@ -20,7 +20,7 @@ describe('nodes.tan', () => {
   test('evaluate', () => {
     const tan = new Tan(new Variable('x'))
     expect(tan.evaluate({ x: 1 })).toBe(Math.tan(1))
-    expect(tan.evaluate({ x: Math.PI })).toBe(0)
+    expect(tan.evaluate({ x: Math.PI })).toBeCloseTo(0)
   })
 })
 
@@ -28,7 +28,7 @@ describe('nodes.csc', () => {
   test('evaluate', () => {
     const csc = new Csc(new Variable('x'))
     expect(csc.evaluate({ x: 1 })).toBe(1 / Math.sin(1))
-    expect(csc.evaluate({ x: Math.PI / 2 })).toBe(1)
+    expect(csc.evaluate({ x: Math.PI / 2 })).toBeCloseTo(1)
   })
 })
 
@@ -36,13 +36,13 @@ describe('nodes.sec', () => {
   test('evaluate', () => {
     const sec = new Sec(new Variable('x'))
     expect(sec.evaluate({ x: 1 })).toBe(1 / Math.cos(1))
-    expect(sec.evaluate({ x: Math.PI })).toBe(-1)
+    expect(sec.evaluate({ x: Math.PI })).toBeCloseTo(-1)
   })
 })
 
 describe('nodes.cot', () => {
   test('evaluate', () => {
     const cot = new Cot(new Variable('x'))
-    expect(cot.evaluate({ x: 1 })).toBe(1 / Math.tan(1))
+    expect(cot.evaluate({ x: 1 })).toBeCloseTo(1 / Math.tan(1))
   })
 })
